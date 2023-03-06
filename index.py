65f51b49e9ff7ed6c4a174ea57712c80fbadb49e
ぐれべ <113990356+FuguFX@users.noreply.github.com>
2023-03-07 06:41:30 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
+apple_price = 200
 
-print('ぐれべ')
-input_one = input('一つ目の数字を入力してください')
-input_two = input('二つ目の数字を入力してください')
-input_calculation = input('何算をしますか（例 たし算)')
-if input_calculation == たしざん：
-  answer = int(input_one) + int(input_two)
-  print(str(input_one) + str(input_two) = str(answer))
-elif
+# input を用いて入力を受け取り、変数 input_count に代入してください
+input_count = input('購入するりんごの個数を入力してください：')
+
+# input_count を数値として代入してください
+count = int(input_count)
+total_price = apple_price * count
+
+print('購入するりんごの個数は' + str(count) + '個です')
+print('支払い金額は' + str(total_price) + '円です')
