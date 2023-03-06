49f253ef191d08eb9c6a831f58416a935f812622
ぐれべ <113990356+FuguFX@users.noreply.github.com>
2023-03-07 06:31:30 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 input_one = input('一つ目の数字を入力してください')
 input_two = input('二つ目の数字を入力してください')
 input_calculation = input('何算をしますか（例 たし算)')
+if input_calculation == たしざん：
+  answer = int(input_one) + int(input_two)
+  print(str(input_one) + str(input_two) = str(answer))
+elif
